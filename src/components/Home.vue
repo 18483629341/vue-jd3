f4ff7fe5c4f1b9493a38581e378889a2b59a5dc9
lisa091500 <714402934@qq.com>
2018-11-06 18:32:00 +0800

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,15 @@
     <div>
         <HomeHeaderView></HomeHeaderView>
         <banner></banner>
-        <!--<HomeNavView></HomeNavView>
-        <HomeMainView></HomeMainView>-->
+        <HomeNavView></HomeNavView>
+        <!--<HomeMainView></HomeMainView>-->
     </div>
 </template>
 
 <script>
 import banner from './banner'
 import HomeHeaderView from './HomeHeader';
-//import HomeNavView from './HomeNav';
+import HomeNavView from './HomeNav';
 //import HomeMainView from './HomeMain';
 import list from './list'
 export default { 
@@ -22,7 +22,7 @@ export default {
     components: {
        banner,
         HomeHeaderView,
-       // HomeNavView,
+        HomeNavView,
         //HomeMainView
     }
 }
