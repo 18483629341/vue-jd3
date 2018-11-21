5a016945d626bfa30de0b2441652b9f522d3be4d
lisa091500 <714402934@qq.com>
2018-11-21 16:03:35 +0800

    index/server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-      name      : 'API',      //应用名
-      script    : 'app.js',   //应用文件位置
+      name      : 'index',      //应用名
+      script    : './config/index.js',   //应用文件位置
       env: {
           PM2_SERVE_PATH: ".",    //静态服务路径
           PM2_SERVE_PORT: 8080,   //静态服务器访问端口
@@ -17,11 +17,31 @@ module.exports = {
       merge_logs: true,         //集群情况下，可以合并日志
       log_type:"json",          //日志类型
       log_date_format: "DD-MM-YYYY",  //日志日期记录格式
-  }],
+  },
+  {
+    name      : 'server',      //应用名
+    script    : 'server.js',   //应用文件位置
+    env: {
+        PM2_SERVE_PATH: ".",    //静态服务路径
+        PM2_SERVE_PORT: 8080,   //静态服务器访问端口
+        NODE_ENV: 'development' //启动默认模式
+    },
+    env_production : {
+        NODE_ENV: 'production'  //使用production模式 pm2 start ecosystem.config.js --env production
+    },
+    instances:"max",          //将应用程序分布在所有CPU核心上,可以是整数或负数
+    watch:true,               //监听模式
+    output: './out.log',      //指定日志标准输出文件及位置
+    error: './error.log',     //错误输出日志文件及位置，pm2 install pm2-logrotate进行日志文件拆分
+    merge_logs: true,         //集群情况下，可以合并日志
+    log_type:"json",          //日志类型
+    log_date_format: "DD-MM-YYYY",  //日志日期记录格式
+},
+],
   deploy : {
       production : {
           user : 'node',                      //ssh 用户
-          host : '212.83.163.1',              //ssh 地址
+          host : '106.12.206.184',              //ssh 地址
           ref  : 'origin/master',             //GIT远程/分支
           repo : 'git@github.com:repo.git',   //git地址
           path : '/var/www/production',       //服务器文件路径
